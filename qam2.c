7e9af08cdacc8c207b9711d7c0fbb35924f61b08
Ewa Januszewska <ejanuszewska@onet.pl>
2017-05-17 10:04:25 +0200

    Nowa zamiana kolejności w tablicy - też nie działa

--- qam2.c
+++ qam2.c
@@ -5,6 +5,15 @@
 #define SIGNAL_LENGTH 4
 #define SWAP(x, y) do {int SWAP = x; x = y; y = SWAP;} while(0)
 
+unsigned int reverse(unsigned int x){
+x = ((x >> 1) & 0x55555555u) | ((x & 0x55555555u) << 1);
+x = ((x >> 2) & 0x33333333u) | ((x & 0x33333333u) <<2);
+x = ((x >> 4) & 0x0f0f0f0fu) | ((x & 0x0f0f0f0fu) << 4);
+x = ((x >> 8) & 0x00ff00ffu) | ((x & 0x00ff00ffu) << 8);
+x = ((x >> 16) & 0xffffu) | ((x & 0xffffu) << 16);
+return x;
+}
+
 int modulateI(uint8_t signal){
   if(signal == 0){
     return -3;
@@ -36,7 +45,7 @@ int main(int argc, char **argv){
 int modulatedSignal[SIGNAL_LENGTH * 2];
 uint8_t  inputSignal[SIGNAL_LENGTH] = {1, 3, 4, 0};
 uint8_t dividedSignal[SIGNAL_LENGTH * 2];
-int j=0;
+int j=1;
 
 for(int i=0; i<SIGNAL_LENGTH; i++){
   dividedSignal[j] = inputSignal[i] >> 2;
@@ -58,24 +67,41 @@ for(int i=0; i< SIGNAL_LENGTH * 2; i++){
 
 int n = SIGNAL_LENGTH;
 int m, w, wm, t, u, b;
-int nn = (n*2);
-int a = 0;
+int nn = (n*2) << 1;
+/*int a = 0;
 
-for(int i = 0; i < (nn); i+=2){
+for(int i = 1; i < nn; i+=2){
   if (a > i){
-    SWAP(modulatedSignal[a], modulatedSignal[a]);
-    SWAP(modulatedSignal[a+1], modulatedSignal[i+1]);
+    SWAP(modulatedSignal[a-1], modulatedSignal[i-1]);
+    SWAP(modulatedSignal[a], modulatedSignal[i]);
  }
-  b = n;
+  b = n*2;
   while (b >= 2 && a > b){
     a -= b;
-    b = b/2;
+    //b = b/2;
+    b >>= 1;
   }  
   a += b;
 }
+*/
+
 printf("po zamianie\n");
-for(int i=0;i<n*2;i++){
-printf("%d\n", modulatedSignal[i]);
+for(unsigned int i=0;i<n*2;i++){
+unsigned int x=i;
+x = ((x >> 1) & 0x55555555u) | ((x & 0x55555555u) << 1);
+x = ((x >> 2) & 0x33333333u) | ((x & 0x33333333u) <<2);
+x = ((x >> 4) & 0x0f0f0f0fu) | ((x & 0x0f0f0f0fu) << 4);
+x = ((x >> 8) & 0x00ff00ffu) | ((x & 0x00ff00ffu) << 8);
+x = ((x >> 16) & 0xffffu) | ((x & 0xffffu) << 16);
+if(x > i){
+//int temp = modulatedSignal[i];
+//modulatedSignal[i] = modulatedSignal[x];
+//modulatedSignal[x] = temp;
+}
+}
+
+for (int i=0; i<n*2; i++){
+printf("%d", modulatedSignal[i]);
 }
 for(int i = 1; i <= log(n); i++){
   m = pow(2, i);
